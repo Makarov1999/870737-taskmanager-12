a83b8d2e31bbbfeaebc04660f7835a2a4241c3b2
egor <makarov.e1999@yandex.ru>
2020-07-29 01:02:01 +0500

    strin added

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const TASK_COUNT = 3;
 const DEFAULT_POSITION = `beforeend`;
 const mainElementOnSite = document.querySelector(`.main`);
 const mainControlElementOnSite = mainElementOnSite.querySelector(`.main__control`);
-
+let tasksList = ``;
 const renderElement = (parent, template, position) => {
   parent.insertAdjacentHTML(position, template);
 };
@@ -374,7 +374,8 @@ const boardTaskList = boardElement.querySelector(`.board__tasks`);
 renderElement(boardTaskList, createFormTaskTemplate(), DEFAULT_POSITION);
 
 for (let i = 0; i < TASK_COUNT; i++) {
-  renderElement(boardTaskList, createTaskCardTemlate(), DEFAULT_POSITION);
+  tasksList+=createTaskCardTemlate();
 }
 
+renderElement(boardTaskList, tasksList, DEFAULT_POSITION);
 renderElement(boardElement, createLoadMoreButtonTemplate(), DEFAULT_POSITION);
